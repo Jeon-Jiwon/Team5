02254af7c3043256bd01c73649584e031d6140bd
woogyeongsik <ychu123@naver.com>
2019-12-20 16:19:22 +0900

    GoodNight

---
+++ src/GoodNight.java
@@ -0,0 +1,10 @@
+
+public class GoodNight {
+	String Night = "goodNight";
+	
+	
+	public static void main(String[] args) {
+		GoodNight nh = new GoodNight ();
+		System.out.println(nh.Night);
+	}
+}
